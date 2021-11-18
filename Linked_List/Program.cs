676e58be75c216e418c69c58d309ee53a5e30b44
Mrhaltung <dipeshchandekar99@gmail.com>
2021-11-18 22:58:38 +0530

    Insert 40 after 30 to the Linked List

--- Linked_List/Program.cs
+++ Linked_List/Program.cs
@@ -9,10 +9,10 @@ namespace Linked_List
             Console.WriteLine("Welcome to Linked List Program.");
             LinkedList List = new LinkedList();
             List.Add(56);
-            //List.Add(30);
+            List.Add(30);
             List.Add(70);
             List.Display();
-            List.insert(1, 30);
+            List.insert(2, 40);
             List.Display();
         }
     }
